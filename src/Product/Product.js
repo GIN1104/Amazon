a8b7159345afddf049001149a92224eaa8bf6ce5
Evgeny Starchenko <evgeny@experahub.com>
2021-08-24 22:09:39 +0300

    Product

--- src/Product/Product.js
+++ src/Product/Product.js
@@ -1,10 +1,25 @@
 import React from 'react';
-import './Product.css'
+import './Product.css';
+import StarIcon from '@material-ui/icons/Star';
 
-function Product() {
+function Product({title, image, price, rating}) {
     return (
         <div className="product">
-            Product
+            <div className="product__info">
+                <p>The lean startup</p>
+                <p className="product__price">
+                <small>$</small>
+                    <strong>19.99</strong>
+                </p>
+                <div className="product__rating">
+                    <p><StarIcon style={{ color: "gold" }} /></p>
+                    <p><StarIcon style={{ color: "gold" }} /></p>
+                    <p><StarIcon style={{color: "gold"}}/></p>
+                </div>
+            </div>
+            <img src="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg" alt="" />
+            
+            <button>Add to Basket</button>
         </div>
     )
 }
