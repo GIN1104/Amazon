ba956715477ed3c2bf16ab17f5d2e42bb629356e
Evgeny Starchenko <starchenkogin@gmail.com>
2021-08-25 11:27:18 +0300

    Product

--- src/Product/Product.js
+++ src/Product/Product.js
@@ -2,22 +2,28 @@ import React from 'react';
 import './Product.css';
 import StarIcon from '@material-ui/icons/Star';
 
-function Product({title, image, price, rating}) {
+function Product({id, title, image, price, rating}) {
     return (
         <div className="product">
             <div className="product__info">
-                <p>The lean startup</p>
+                <p>{title}</p>
                 <p className="product__price">
-                <small>$</small>
-                    <strong>19.99</strong>
+                    <small>$</small>
+                    <strong>{price}</strong>
                 </p>
                 <div className="product__rating">
-                    <p><StarIcon style={{ color: "gold" }} /></p>
-                    <p><StarIcon style={{ color: "gold" }} /></p>
-                    <p><StarIcon style={{color: "gold"}}/></p>
+                    {
+                        Array(rating)
+                            .fill()
+                            .map((_, i) => (<p><StarIcon
+                                style={{
+                                    color: "gold"
+                                }}/></p>))
+                    }
+
                 </div>
             </div>
-            <img src="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg" alt="" />
+            <img src={image} alt=""/>
             <button>Add to Basket</button>
         </div>
     )
