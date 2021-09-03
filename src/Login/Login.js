e85e9addb20606ad278afded8b53443fb07efef9
Evgeny Starchenko <starchenkogin@gmail.com>
2021-09-03 18:12:33 +0300

    Login page

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from 'react';
+import "./Login.css";
+import {Link} from "react-router-dom";
 
 function Login() {
     return (
-        <div>
-            Login Page
+        <div className="login">
+            <Link to="/">
+                <img
+                    className="login__logo"
+                    alt="Amazon logo"
+                    src="https://pngimg.com/uploads/amazon/amazon_PNG24.png"
+                />
+            </Link>
+            <div className="login__container">
+                <h1>Sign-in</h1>
+                <form>
+                    <h5>E-mail</h5>
+                    <input type="text" />
+                    <h5>Password</h5>
+                    <input type="password" />
+                    <button className="login__signInButton">Sign In</button>
+                </form>
+                <p>
+                    By signing-in you agree to the AMAZON FAKE Conditions of Use & Sale. Please see our Privacy Notice, our Cookies Notice and our Interest-Based Ads Notice.
+                </p>
+                <button className="login_registerButton">Create yuor amazon account</button>
+            </div>
         </div>
     )
 }
