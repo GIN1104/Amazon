80464a1ffc7b9e8fe9d47321291f27bc6f12df20
Evgeny Starchenko <starchenkogin@gmail.com>
2021-09-06 15:06:56 +0300

    Payment page

--- src/Payment/Payment.js
+++ src/Payment/Payment.js
@@ -1,17 +1,44 @@
 import React from 'react';
-import './Payment.css'
+import CheckoutProduct from '../CheckoutProduct/CheckoutProduct';
+import { useStateValue } from '../StateProvider';
+import './Payment.css';
+
 
 function Payment() {
+    const [{ basket, user }] = useStateValue();
     return (
         <div className="payment">
-            Payment Page
             <div className="payment__container">
                 <div className="payment__section">
                     <div className="payment__title">
                         <h3>Delivery Address</h3>
                     </div>
+                    <div className="payment__address">
+                        <p>{user?.email}</p>
+                        <p>Haifa</p>
+                        <p>Israel</p>
+                    </div>
                 </div>
-                <div className="payment__section"></div>
+                <div className="payment__section">
+                    <div className="payment__title">
+                        <h3>Review items and delivery</h3>
+                    </div>
+                    <div className="payment__items" style={{backgroundColor: 'white'}}>
+                    {
+                        basket.map((item, i )=> (
+                            <CheckoutProduct 
+                            key={i}
+                            id={item.id}
+                            title={item.title}
+                            image={item.image}
+                            price={item.price}
+                            rating={item.rating}
+                        />
+                        ))
+
+                    }
+                    </div>
+                </div>
                 <div className="payment__section"></div>
             </div>
         </div>
