3089477e65c364cc3d9520f3f42fc3305ca7dfdc
Evgeny Starchenko <starchenkogin@gmail.com>
2021-09-06 21:52:51 +0300

    Checkout page

--- src/Payment/Payment.js
+++ src/Payment/Payment.js
@@ -1,20 +1,36 @@
 import React from 'react';
 import CheckoutProduct from '../CheckoutProduct/CheckoutProduct';
-import { useStateValue } from '../StateProvider';
+import {useStateValue} from '../StateProvider';
 import './Payment.css';
-
+import {Link} from 'react-router-dom'
 
 function Payment() {
-    const [{ basket, user }] = useStateValue();
+    const [
+        {
+            basket,
+            user
+        }
+    ] = useStateValue();
     return (
         <div className="payment">
             <div className="payment__container">
+                <h1>
+                    Checkout(
+                    <Link to="/checkout">{
+                            basket?.length
+                        } item</Link>
+                    )
+                </h1>
+
                 <div className="payment__section">
                     <div className="payment__title">
                         <h3>Delivery Address</h3>
                     </div>
                     <div className="payment__address">
-                        <p>{user?.email}</p>
+                        <p>{
+                                user
+                                    ?.email
+                            }</p>
                         <p>Haifa</p>
                         <p>Israel</p>
                     </div>
@@ -23,23 +39,31 @@ function Payment() {
                     <div className="payment__title">
                         <h3>Review items and delivery</h3>
                     </div>
-                    <div className="payment__items" style={{backgroundColor: 'white'}}>
-                    {
-                        basket.map((item, i )=> (
-                            <CheckoutProduct 
-                            key={i}
-                            id={item.id}
-                            title={item.title}
-                            image={item.image}
-                            price={item.price}
-                            rating={item.rating}
-                        />
-                        ))
+                    <div
+                        className="payment__items"
+                        style={{
+                            backgroundColor: 'white'
+                        }}>
+                        {
+                            basket.map((item, i) => (
+                                <CheckoutProduct
+                                    key={i}
+                                    id={item.id}
+                                    title={item.title}
+                                    image={item.image}
+                                    price={item.price}
+                                    rating={item.rating}/>
+                            ))
 
-                    }
+                        }
                     </div>
                 </div>
-                <div className="payment__section"></div>
+                <div className="payment__section">
+                    <div className="payment__title">
+                        <h3>Payment Method</h3>
+                    </div>
+                    <div className="payment__details"></div>
+                </div>
             </div>
         </div>
     )
